9d950fd3748c61703d9e6fe7e4ecdd936ca06916
Rodolfo Simas <rodolfojfsimas@gmail.com>
2023-05-22 15:42:56 -0300

    terceiro ajuste artigo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -329,11 +329,11 @@
                                     <a href="{{ route('aprompt.show', $prompt->id) }}">
                                         <div class="card mb-3" id="card-artigo-ai">
                                             <div class="row g-0">
-                                                <div class="col-md-3">
+                                                <div class="col-3">
                                                     <img src="{{ URL::to('/') }}/storage/{{ $prompt->imagem }}"
                                                         class="img-fluid" alt="..." id="img-card" />
                                                 </div>
-                                                <div class="col-md-9">
+                                                <div class="col-9">
                                                     <div class="card-body" id="card-body-artigo">
                                                         <p class="card-text"><small
                                                                 class="text-body-secondary">{{ $prompt->data }}</small>
