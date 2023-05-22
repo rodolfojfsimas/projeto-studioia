486eb1c895fcebaee1fb596f976b5fa27c36284a
Rodolfo Simas <rodolfojfsimas@gmail.com>
2023-05-22 20:32:31 -0300

    ajuste card

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -331,7 +331,7 @@
                                             <div class="row g-0">
                                                 <div class="col-3">
                                                     <img src="{{ URL::to('/') }}/storage/{{ $prompt->imagem }}"
-                                                        class="img-fluid .mx-auto" alt="..." id="img-card" />
+                                                        class="img-fluid" alt="..." id="img-card" />
                                                 </div>
                                                 <div class="col-9">
                                                     <div class="card-body" id="card-body-artigo">
